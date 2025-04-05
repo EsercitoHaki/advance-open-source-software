cc91528fc8c498bde19653f59159b14f5d140895
EsercitoHaki <esercitohaki2003@gmail.com>
2025-04-05 23:34:32 +0700

    Fix issue in code

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function setPasswordAttribute($value)
     {
-        $this->attributes['password'] = Hash::make($value);
+        $this->attributes['password'] = $value;
     }
 
     // JWT functions
